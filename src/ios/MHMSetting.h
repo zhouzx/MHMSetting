7a10ca30f67e175b713f2bf759933701289b4cdd
zhouzx <zhouzx@cn.ibm.com>
2017-06-01 16:22:57 +0800

    add js file

--- src/ios/MHMSetting.h
+++ src/ios/MHMSetting.h
@@ -9,17 +9,10 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
-#import <NewRelicAgent/NewRelic.h>
 
 @interface MHMSetting : CDVPlugin
 
 - (void) launchMapApp:(CDVInvokedUrlCommand*)command;
 - (void) callHandler:(CDVInvokedUrlCommand*)command;
-- (void) smsHandler:(CDVInvokedUrlCommand*)command;
-- (void) emailHandler:(CDVInvokedUrlCommand*)command;
 
-- (void) newRelicRecordAccessTimesFacetUserID:(CDVInvokedUrlCommand*)command;
-- (void) newRelicRecordWatchLogin:(CDVInvokedUrlCommand*)command;
-- (void) newRelicRecordEvent:(CDVInvokedUrlCommand*)command;
-- (void) newRelicSendFeedback:(CDVInvokedUrlCommand*)command;
 @end
